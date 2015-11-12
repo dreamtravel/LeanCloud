fbfa159b55c95d470bd8e303280384e36e277555
杨欣 <dream98248464@icloud.com>
2015-11-12 13:44:42 +0800

    Initial Commit

---
+++ LeanCloud/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeanCloud
+//
+//  Created by Mac on 15/11/12.
+//  Copyright © 2015年 Macmini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeanCloud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeanCloud
+//
+//  Created by Mac on 15/11/12.
+//  Copyright © 2015年 Macmini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
